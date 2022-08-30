ccef6feea4cf9d591e8f79318a4e5813ecb8c2ac
sebn <sebn@firepot.finance>
2022-08-29 20:32:25 -0500

    .

--- src/features/YieldFarm/YieldFarmView.tsx
+++ src/features/YieldFarm/YieldFarmView.tsx
@@ -4,23 +4,24 @@ import styled from 'styled-components';
 import { TYPE } from 'legacy/theme';
 import YieldFarmCardView from './components/YieldFarmCardView';
 import { SwapPoolTabs } from 'legacy/components/NavigationTabs';
+import useYieldFarmViewModel from './useYieldFarmViewModel';
 
+const PageWrapper = styled.div`
+  padding: 12px 1rem 0px 1.5rem;
+  margin-bottom: 0.4rem;
+  width: 100%;
+  color: ${({ theme }) => theme.text2};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleRow = styled.div`
+  padding: 12px 1rem 0px 1.5rem;
+  margin-bottom: 0.4rem;
+`;
 export default function YieldFarmView() {
-  const PageWrapper = styled.div`
-    padding: 12px 1rem 0px 1.5rem;
-    margin-bottom: 0.4rem;
-    width: 100%;
-    color: ${({ theme }) => theme.text2};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
-  const TitleRow = styled.div`
-    padding: 12px 1rem 0px 1.5rem;
-    margin-bottom: 0.4rem;
-  `;
-
+  const { viewModel } = useYieldFarmViewModel();
   return (
     <AppBody>
       <SwapPoolTabs active={'farm'} />
