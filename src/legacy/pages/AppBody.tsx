447e444622dd96288b7b9584532344967d27c854
Tom Daniel <0xtomdaniel@gmail.com>
2022-09-10 01:04:46 -0600

    Fixed card cutting off at bottom

--- src/legacy/pages/AppBody.tsx
+++ src/legacy/pages/AppBody.tsx
@@ -6,6 +6,7 @@ export const BodyWrapper = styled.div`
   max-width: 500px;
   width: 100%;
   padding: 0.2rem;
+  margin: 0 0 64px 0;
   border-radius: 1.6rem;
   box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px, rgba(0, 0, 0, 0.04) 0px 16px 24px,
     rgba(0, 0, 0, 0.01) 0px 24px 32px;
