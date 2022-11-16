023df44545448d37b344d9b1702f2d69ad76c227
sebn <sebn@firepot.finance>
2022-11-16 12:42:12 -0500

    fix: update tvl on header to take several lp contracts

--- src/features/TVLDisplay/components/TVLView.tsx
+++ src/features/TVLDisplay/components/TVLView.tsx
@@ -9,12 +9,12 @@ const BalanceText = styled(Text)`
   `};
 `;
 export default function TVLView() {
-    //Refactor to grab this from redux state
-    const poolContractAddress = '0x015AB9B3771F1748007ea62885737eF4Fa346291';
-    const { viewModel } = useTVLViewModel(poolContractAddress);
-    return (
-        <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-            $ {viewModel} TVL
-        </BalanceText>
-    );
+  //Refactor to grab this from redux state
+  const poolContractAddress = '0x015AB9B3771F1748007ea62885737eF4Fa346291';
+  const { viewModel } = useTVLViewModel(poolContractAddress);
+  return (
+    <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
+      $ {viewModel} TVL
+    </BalanceText>
+  );
 }
