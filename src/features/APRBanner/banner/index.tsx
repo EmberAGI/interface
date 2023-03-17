8bac43953c66c8446bf9a176fdd476f0f764a492
Ricardo <ralvarezcm@gmail.com>
2023-03-17 14:09:15 -0500

    hotfix: apr banner showing wrong number

--- src/features/APRBanner/banner/index.tsx
+++ src/features/APRBanner/banner/index.tsx
@@ -26,6 +26,8 @@ const APRContainer = styled.div`
 export default function APRBanner() {
   const busdModel = useYieldFarmStatsViewModel('0xa17DdfBCB5D8304835062324D99bfBd1d5cE4841');
   const usdcModel = useYieldFarmStatsViewModel('0x035Cf2b69d439565A812aAf2DfE174c89Ba3e585');
+  console.log(busdModel.viewModel.apr);
+  console.log(usdcModel.viewModel.apr);
 
   return (
     <APRContainer>
@@ -34,7 +36,10 @@ export default function APRBanner() {
           Stake on our high <Link to="/farm">APR Farms</Link>
         </span>
         &nbsp;- APR:{' '}
-        {busdModel.viewModel.apr > usdcModel.viewModel.apr ? busdModel.viewModel.apr : usdcModel.viewModel.apr}%
+        {parseInt(busdModel.viewModel.apr) > parseInt(usdcModel.viewModel.apr)
+          ? busdModel.viewModel.apr
+          : usdcModel.viewModel.apr}
+        %
       </div>
     </APRContainer>
   );
